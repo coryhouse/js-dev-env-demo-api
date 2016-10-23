f2720e8997bfeef15c23cba2c70270271c10cd51
Cory House <housecor@gmail.com>
2016-10-23 10:18:17 -0500

    Move all to root for simplicity

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node dist/index.js
+web: node index.js
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var app = express();
+import express from 'express';
+
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+});
 
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
@@ -14,5 +19,5 @@ app.get('/users', function(req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("Node app is running at localhost:" + app.get('port')); 
 });
